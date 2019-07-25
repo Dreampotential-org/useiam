78ea9b990b518b9dd1850c3e3d3b644cd443bd72
Aaron Rosen <aaronorosen@gmail.com>
2019-07-25 12:36:11 -0700

    more fixes

--- platforms/ios/www/js/video.js
+++ platforms/ios/www/js/video.js
@@ -11,7 +11,6 @@ function init_video_event() {
     });
 
     $("#videoInfo .btnOk").on('click', function(e) {
-        alert("Start take video")
         var captureSuccess = function(mediaFiles) {
             var i, path, len;
             for (i = 0, len = mediaFiles.length; i < len; i += 1) {
@@ -36,42 +35,74 @@ function init_video_event() {
 
 
 function api_video_checkin(mediaFile) {
+    swal({
+        title: "0%",
+        text: "Video uploading please wait.",
+        icon: "info",
+        buttons: false,
+        closeOnEsc: false,
+        closeOnClickOutside: false,
+    });
     function win(r) {
         console.log("Code = " + r.responseCode);
         console.log("Response = " + r.response);
         console.log("Sent = " + r.bytesSent);
-        alert("Finished!");
+        swal({
+          title: "Good job!",
+          text: "Video submitted successfully!",
+          icon: "success",
+        });
     }
+
     function fail(error) {
-       
+        console.log(error);
+        alert("An error has occurred: Code = " + error.code);
         console.log("upload error source " + error.source);
         console.log("upload error target " + error.target);
-        alert("An error has occurred: Code = " + error.code);
     }
+
     var uri = encodeURI(SERVER + "/api/video-upload/");
     var options = new FileUploadOptions();
     options.fileKey = "video";
-    options.fileName = mediaFile.name;
-    options.mimeType = mediaFile.type;
+    options.fileName = mediaFile.name
+    options.mimeType = mediaFile.type
     options.contentType = "multipart/form-data";
-
     options.httpMethod = "POST";
     options.chunkedMode = false;
     var headers = {
         'Authorization': "Token " + localStorage.getItem("session_id")
     };
+
     options.headers = headers;
-    var ft = new FileTransfer();
-    ft.onprogress = function(progressEvent) {
-        console.log(progressEvent);
-        if (progressEvent.lengthComputable) {
-            // do not open this comment because we donot have loadingstatus
-            // loadingStatus.setPercentage(progressEvent.loaded / progressEvent.total);
-            console.log(progressEvent.loaded / progressEvent.total);
-        } else {
-            // loadingStatus.increment();
-        }
-    };
-    // alert(mediaFile.fullPath)
-    ft.upload(mediaFile.fullPath, uri, win, fail, options);
+    var type = window.PERSISTENT;
+   var size = 500*1024*1024;//500 MB
+   var ft = new FileTransfer();
+
+    window.requestFileSystem(type, size, successCallback, errorCallback); //Request Access file system permission
+    function successCallback(fs) {
+       fs.root.getFile("DCIM/Camera/"+mediaFile.name,{ create: false, exclusive: false }, function(fileEntry) {
+          fileEntry.file(function(file) {
+             ft.onprogress = function(progressEvent) {
+                $(".swal-title").text(parseInt(progressEvent.loaded/progressEvent.total*100) + "%");
+                   return;
+            };
+            ft.upload(mediaFile.fullPath, uri, win, fail, options);
+          }, errorCallbackFileEntry);
+       }, errorCallbackGetFile);
+    }
+ 
+    function errorCallbackGetFile(error) {
+        console.log("errorCallbackGetFile ",error);
+       alert("ERROR: " + error.code)
+    }
+
+    function errorCallbackFileEntry(error) {
+        console.log("errorCallbackFileEntry ",error);
+       alert("ERROR: " + error.code)
+    }
+    function errorCallback(error) {
+        console.log("errorCallback ",error);
+       alert("ERROR: " + error.code)
+    }
+
 }
