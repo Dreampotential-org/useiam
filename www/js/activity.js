0ad148872f6f22fc6491cd88b1f63c1c35d12b9e
Aaron Rosen <aaronorosen@gmail.com>
2019-08-13 16:37:13 -0700

    activity log

--- www/js/activity.js
+++ www/js/activity.js
@@ -8,7 +8,8 @@ function init_activity() {
             display_activities(resp.events)
         });
     })
-    /* $("#viewActivity").click()
+    /*
+    $("#viewActivity").click()
     $(".toggleBar").click()
     get_activity(function(resp) {  display_activities(resp.events) });
     */
@@ -37,23 +38,35 @@ function get_activity(callback) {
     });
 }
 
+function formatDate(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return date.toLocaleDateString("en-US") + " " + strTime;
+}
+
+
 function display_activities(activities) {
     for (var activity of activities) {
         if (activity.type == 'gps') {
             $(".activity-log").append(
                 "<div><span>" + activity.type + "</span> - " +
                     "<span>" + activity.msg + "</span> - " +
-                    "<span>" + new Date(activity.created_at*1000) + "</span></div>"
+                    "<span>" + formatDate(new Date(activity.created_at*1000)) + "</span></div>"
             )
         }
         if (activity.type == 'video') {
             $(".activity-log").append(
                 "<div><span>" + activity.type + "</span> - " +
-                    "<span><a url=" +
+                    "<span><a href=" +
                             activity.url +
-                        " href='#'>Play</a></span> - " +
+                        ">Play</a></span> - " +
                     "<span>" +
-                        new Date(activity.created_at * 1000) +
+                        formatDate(new Date(activity.created_at * 1000)) +
                     "</span></div>"
             )
         }
