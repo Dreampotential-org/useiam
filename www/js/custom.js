3fd04c9b1a655dbfc900f6bedcb71866f700a15d
Aaron Rosen <aaronorosen@gmail.com>
2019-08-15 10:46:08 -0700

    Add beach comber

--- www/js/custom.js
+++ www/js/custom.js
@@ -20,32 +20,6 @@ function init_reset_password() {
 }
 
 
-function init_logo() {
-    var full = window.location.host
-    //window.location.host is subdomain.domain.com
-    var parts = full.split('.')
-    var sub = parts[0]
-    var domain = parts[1]
-    var type = parts[2]
-
-    if (sub == 'futures') {
-        $(".logo").append("<img src='img/futuresLogo.png' />")
-    } else if (sub == 'pbbhw') {
-        $(".logo").append("<img src='img/pbbhw.png' />")
-        $(".logo img").css("width", '327px')
-    } else if (sub == 'urbanrecovery') {
-        $(".logo").append("<img src='img/urbanRecover.png' />")
-        $(".logo img").css("width", '220px')
-        $(".logo img").css("padding-top", '22px')
-    } else {
-        $(".logo").append("<img src='images/iam.png' />")
-        $(".logo img").css("width", '100px')
-    }
-
-    $(".logo img").show()
-}
-
-
 // capture all errors and send to slack
 window.onerror = function (msg, url, lineNo, columnNo, error) {
     var string = msg.toLowerCase();
---
+++ www/js/logos.js
@@ -0,0 +1,28 @@
+function init_logo() {
+    var full = window.location.host
+    //window.location.host is subdomain.domain.com
+    var parts = full.split('.')
+    var sub = parts[0]
+    var domain = parts[1]
+    var type = parts[2]
+
+    if (sub == 'futures') {
+        $(".logo").append("<img src='img/futuresLogo.png' />")
+
+    } else if (sub == 'beachcomber') {
+        $(".logo").append("<img src='img/beachlogo.png' />")
+        $(".logo img").css("width", '200px')
+    } else if (sub == 'pbbhw') {
+        $(".logo").append("<img src='img/pbbhw.png' />")
+        $(".logo img").css("width", '327px')
+    } else if (sub == 'urbanrecovery') {
+        $(".logo").append("<img src='img/urbanRecover.png' />")
+        $(".logo img").css("width", '220px')
+        $(".logo img").css("padding-top", '22px')
+    } else {
+        $(".logo").append("<img src='images/iam.png' />")
+        $(".logo img").css("width", '100px')
+    }
+
+    $(".logo img").show()
+}
