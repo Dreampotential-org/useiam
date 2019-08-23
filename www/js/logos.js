68d1932edc2f4371dfa8f14c0a9e094e7dc96052
Aaron Rosen <aaronorosen@gmail.com>
2019-08-23 12:42:45 -0700

    pineland

--- www/js/logos.js
+++ www/js/logos.js
@@ -8,6 +8,9 @@ function init_logo() {
 
     if (sub == 'futures') {
         _append_logo_and_icon("img/futuresLogo.png")
+    } else if (sub == 'pinelands') {
+        _append_logo_and_icon("img/pinelands.png")
+        $(".logo img").css("width", '200px')
     } else if (sub == 'valleyrecovery') {
         _append_logo_and_icon("img/valley.png")
         $(".logo img").css("width", '126px')
