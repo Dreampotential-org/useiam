d7a68b32f0a5c3efbaa57737e8a52e3277e8d0b8
instantwebsitedevelopment <instantwebsitedevelopment@gmail.com>
2020-07-08 23:33:46 +0530

    Client list page changes

---
+++ www/js/clients.js
@@ -0,0 +1,72 @@
+function init() {
+    list_patients(function(response) {
+        display_patients(response.patients);
+    })
+    $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".clientsList .col-2").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
+}
+
+function list_patients(callback) {
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+       "headers": {
+       "Authorization": "Token " + localStorage.getItem("session_id"),
+      },
+      "url": SERVER + "/api/list-patients/",
+      "method": "GET",
+      "processData": false,
+      "contentType": false,
+      "mimeType": "multipart/form-data",
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response)
+        callback(JSON.parse(response))
+    }).fail(function(err) {
+        console.log(err)
+    });
+}
+
+function display_patients(patients) {
+    /*$(".select-patient").append(
+        "<option value=''>All Patients</option>"
+    )
+
+    for(var patient of patients) {
+        $(".select-patient").append(
+            "<option value='" + patient.email + "'>" +
+                patient.name + "(" + patient.email +  ")" +
+            "</option>"
+        )
+    }*/
+    for(var patient of patients) {
+        $(".clientsList").append(
+            '<div class="col-2">'+
+                '<div class="card-header font-weight-bold clientInfo-cardHeader">'+
+                    '<div class="clientInfo">'+
+                        '<div class="clinetProfileImage">'+
+                            '<i class="material-icons align-middle mr-2">person</i>'+
+                        '</div>'+
+                        '<div class="clientName"><span>'+patient.name+'</span></div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'
+        )
+    }
+    var classes = ['bg-1', 'bg-2', 'bg-3', 'bg-4', 'bg-5', 'bg-6'];
+    var len = $(".clinetProfileImage").length;
+    $(".clinetProfileImage").each(function(index){
+        var random = Math.floor( Math.random() * len ) + 1;
+        random = (Math.random() * len--) > 6 ? random + 1 : random;
+        //alert(random);
+        $(this).addClass(classes[random]);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
