0c80ac00695ffa0df3426aa0e9ee11a0e09a4ae7
websitedesignanddeveloper <websitedesignanddeveloper@gmail.com>
2020-07-15 01:35:55 +0530

    Worked on single page app

--- www/js/clients.js
+++ www/js/clients.js
@@ -45,21 +45,42 @@ function display_patients(patients) {
             "</option>"
         )
     }*/
+    // for(var patient of patients) {
+    //     $(".clientsList").append(
+    //         '<div class="col-2">'+
+    //             '<div class="card-header font-weight-bold clientInfo-cardHeader">'+
+    //                 '<div class="clientInfo">'+
+    //                     '<div class="clinetProfileImage">'+
+    //                         '<i class="material-icons align-middle mr-2">person</i>'+
+    //                     '</div>'+
+    //                     '<div class="clientName"><span>'+patient.name+'</span></div>'+
+    //                 '</div>'+
+    //             '</div>'+
+    //         '</div>'
+    //     )
+    // }
+
+
     for(var patient of patients) {
         $(".clientsList").append(
-            '<div class="col-2">'+
-                '<div class="card-header font-weight-bold clientInfo-cardHeader">'+
-                    '<div class="clientInfo">'+
-                        '<div class="clinetProfileImage">'+
-                            '<i class="material-icons align-middle mr-2">person</i>'+
-                        '</div>'+
-                        '<div class="clientName"><span>'+patient.name+'</span></div>'+
-                    '</div>'+
-                '</div>'+
-            '</div>'
+
+            `<div class="col-md-3 col-lg-2 col-sm-3 col-6 my-2">
+
+                <div class="card">
+                    <div class="text-center bg-secondary d-flex justify-content-center align-items-center clinetProfileImage">
+                        <i style="font-size: 100px;" class="material-icons">person</i>
+                    </div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle">${patient.name}</h6>
+                    </div>
+                </div>
+            </div>`
         )
     }
-    var classes = ['bg-1', 'bg-2', 'bg-3', 'bg-4', 'bg-5', 'bg-6'];
+
+
+    //var classes = ['bg-1', 'bg-2', 'bg-3', 'bg-4', 'bg-5', 'bg-6'];
+    var classes = ['bg-primary', 'bg-secondary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info'];
     var len = $(".clinetProfileImage").length;
     $(".clinetProfileImage").each(function(index){
         var random = Math.floor( Math.random() * len ) + 1;
