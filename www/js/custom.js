c8bce3168a6b438325c8822d3bc12778494e700d
Aaron Rosen <aaronorosen@gmail.com>
2020-08-19 18:17:28 -0700

    remove this part

--- www/js/custom.js
+++ www/js/custom.js
@@ -39,12 +39,8 @@ function block_desktop() {
         title: "Computer not supported",
         text: "Use IAM with your smartphone or tablet.",
         icon: "info",
-        buttons: {cancel: "Admin Interface"},
         closeOnEsc: false,
         closeOnClickOutside: false,
-    }).then((value) => {
-        console.log("test");
-        window.location = "new_index.html"
     });
     }
 }
