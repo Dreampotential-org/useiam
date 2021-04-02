20b36b9758f1bea5e579f4c357e48c56eb84bb13
Farrukh <farrukhrazzaqbutt@gmail.com>
2021-04-02 20:36:29 +0500

    production URLs

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
-var SERVER = 'http://127.0.0.1:8001'
-// var SERVER = 'https://prod-api.useiam.net'
-var PythonAPIServer = 'http://127.0.0.1:8000'
-// var PythonAPIServer = 'https://api.dreampotential.org'
+// var SERVER = 'http://127.0.0.1:8001'
+var SERVER = 'https://prod-api.useiam.net'
+// var PythonAPIServer = 'http://127.0.0.1:8000'
+var PythonAPIServer = 'https://api.dreampotential.org'
 
 // plan id = IAM001
 // test plan id = 007
