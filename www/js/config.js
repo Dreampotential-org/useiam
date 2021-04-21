daa47ae9f9d2a9f26a04e38d361e12a75215d590
Aaron Rosen <aaronorosen@gmail.com>
2021-04-21 12:09:06 -0700

    Revert "magic link"
    This reverts commit 0546f47133fdf262235d2428cbd0239359d8e777.

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,8 @@
 // var SERVER = 'http://127.0.0.1:8001'
-var SERVER = 'https://prod-api.useiam.net';
+var SERVER = 'https://prod-api.useiam.net'
+var SERVER_API = 'http://192.168.100.141:8080'
 // var PythonAPIServer = 'http://127.0.0.1:8000'
-var PythonAPIServer = 'https://api.dreampotential.org';
+var PythonAPIServer = 'https://api.dreampotential.org'
 
 // plan id = IAM001
 // test plan id = 007
