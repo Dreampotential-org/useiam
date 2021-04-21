2903a125b791972110fbeeebd5963c5d4afa5dea
cipher117 <mianwaqarmuneer883@gmail.com>
2021-04-21 14:36:50 +0500

    git issue

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,6 @@
 // var SERVER = 'http://127.0.0.1:8001'
 var SERVER = 'https://prod-api.useiam.net'
+var SERVER_API = 'http://192.168.100.141:8080'
 // var PythonAPIServer = 'http://127.0.0.1:8000'
 var PythonAPIServer = 'https://api.dreampotential.org'
 
