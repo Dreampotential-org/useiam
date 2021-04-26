359a453dd3257239cc7c45cd652338f85fae3016
webdevelopersbc <webdevelopersbc@gmail.com>
2021-04-26 10:29:32 +0530

    fixed the console errors

--- www/js/iap.js
+++ www/js/iap.js
@@ -1,159 +1,155 @@
 function init_iap_events() {
-    init_unsubscribe_events();
+  init_unsubscribe_events();
 
-    $(".subscribe").on("click", function (e) {
-      console.log("subscribe clicked ######");
-      if (false && isApp())
-      {
-        $("#subscriptionModule").addClass("is-visible");
-      }
-      else if (false && window.cordova && (
-        window.cordova.platformId == "ios" ||
-        window.cordova.platformId == "android")
-      )
-      {
-        // APPLE PAY
-        console.log("showtab else");
-        store.when("base_subscription_7").approved(finishPurchase);
-        store.register({ type: store.CONSUMABLE, id: "base_subscription_7" });
-        store.refresh();
-        store.order("base_subscription_7");
-      }
-      // var SERVER_subscription = 'http://127.0.0.1:8000'
-      var settings_client_token = {
-        async: true,
-        crossDomain: true,
-        // headers: {
-        //   Authorization: "Token " + localStorage.getItem("session_id"),
-        // },
-        url: "https://api.dreampotential.org/store/userSubscribe",
-        method: "GET",
-        processData: false,
-        contentType: false,
-        mimeType: "multipart/form-data",
-      };
+  $(".subscribe").on("click", function (e) {
+    console.log("subscribe clicked ######");
+    if (false && isApp()) {
+      $("#subscriptionModule").addClass("is-visible");
+    }
+    else if (false && window.cordova && (
+      window.cordova.platformId == "ios" ||
+      window.cordova.platformId == "android")
+    ) {
+      // APPLE PAY
+      console.log("showtab else");
+      store.when("base_subscription_7").approved(finishPurchase);
+      store.register({ type: store.CONSUMABLE, id: "base_subscription_7" });
+      store.refresh();
+      store.order("base_subscription_7");
+    }
+    // var SERVER_subscription = 'http://127.0.0.1:8000'
+    var settings_client_token = {
+      async: true,
+      crossDomain: true,
+      // headers: {
+      //   Authorization: "Token " + localStorage.getItem("session_id"),
+      // },
+      url: "https://api.dreampotential.org/store/userSubscribe",
+      method: "GET",
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+    };
 
-      $.ajax(settings_client_token)
-        .done(function (response) {
-          var resp = JSON.parse(response);
-          console.log(resp);
-          $("#client_token").val(resp.client_token);
-          // subscription start
-          var form = document.querySelector("#subscription_form");
-          var client_token = $("#client_token").val()
+    $.ajax(settings_client_token)
+      .done(function (response) {
+        var resp = JSON.parse(response);
+        console.log(resp);
+        $("#client_token").val(resp.client_token);
+        // subscription start
+        var form = document.querySelector("#subscription_form");
+        var client_token = $("#client_token").val()
 
-          braintree.dropin.create(
+        braintree.dropin.create(
           {
-              authorization: client_token,
-              container: "#bt-dropin",
-              paypal: {
+            authorization: client_token,
+            container: "#bt-dropin",
+            paypal: {
               flow: "vault",
-              },
+            },
           },
           function (createErr, instance) {
-              form.addEventListener("submit", function (event) {
+            form.addEventListener("submit", function (event) {
               event.preventDefault();
               instance.requestPaymentMethod(function (err, payload) {
-                    console.log(err)
-                  if (err) {
-                        swal({title: "Error!",
-                              text: err.message,
-                              icon: 'error'})
-                        console.log("Error", err);
-                        return;
-                  }
-                  $('#subsciptionSubmit').hide();
+                console.log(err)
+                if (err) {
+                  swal({
+                    title: "Error!",
+                    text: err.message,
+                    icon: 'error'
+                  })
+                  console.log("Error", err);
+                  return;
+                }
+                $('#subsciptionSubmit').hide();
 
-              // Add the nonce to the form and submit
-              document.querySelector("#nonce").value = payload.nonce;
-              // form.submit();
-              ///////
-              var subscription_form = new FormData();
-              subscription_form.append("payment_method_nonce", $("#nonce").val())
-              // subscription_form.append("client_token", $("#client_token").val())
-              subscription_form.append("session_id", localStorage.getItem("session_id"))
-              subscription_form.append("subscription_plan_ID", $("#subscription_plan_ID").val())
-              var settings_add_item_update = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://api.dreampotential.org/store/userbrainTreeSubscription",
-                "method": "POST",
-                "type": "POST",
-                "processData": false,
-                "contentType": false,
-                "mimeType": "multipart/form-data",
-                "data": subscription_form,
-                // "headers": {
-                //     "Authorization": localStorage.getItem("user-token")
-                // }
-              };
-              $.ajax(settings_add_item_update).done(function (response) {
-                response = JSON.parse(response);
-                do_set_paying(response.ID);
-                swal({
-                  title: "Subscription",
-                  text: "Subscrition Successful",
-                  icon: "success",
+                // Add the nonce to the form and submit
+                document.querySelector("#nonce").value = payload.nonce;
+                // form.submit();
+                ///////
+                var subscription_form = new FormData();
+                subscription_form.append("payment_method_nonce", $("#nonce").val())
+                // subscription_form.append("client_token", $("#client_token").val())
+                subscription_form.append("session_id", localStorage.getItem("session_id"))
+                subscription_form.append("subscription_plan_ID", $("#subscription_plan_ID").val())
+                var settings_add_item_update = {
+                  "async": true,
+                  "crossDomain": true,
+                  "url": "https://api.dreampotential.org/store/userbrainTreeSubscription",
+                  "method": "POST",
+                  "type": "POST",
+                  "processData": false,
+                  "contentType": false,
+                  "mimeType": "multipart/form-data",
+                  "data": subscription_form,
+                  // "headers": {
+                  //     "Authorization": localStorage.getItem("user-token")
+                  // }
+                };
+                $.ajax(settings_add_item_update).done(function (response) {
+                  response = JSON.parse(response);
+                  do_set_paying(response.ID);
+                  swal({
+                    title: "Subscription",
+                    text: "Subscrition Successful",
+                    icon: "success",
+                  });
+                });
+              }
+              );
+              $("#subscriptionModule").addClass("is-visible");
+            })
+              .fail(function (err) {
+                console.log(err);
               });
-              });
-          }
-          );
-
-          $("#subscriptionModule").addClass("is-visible");  
-        })
-        .fail(function (err) {
-          console.log(err);
-        });
-
+          });
+      });
     });
-}
+
+    // <!-- script for toggling display of the form -->
+
+    function toggleDisplay() {
+      var x = document.getElementById("collapseStripe");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        s
+        x.style.display = "none";
+      }
+    }
 
 
+    function finishPurchase(p) {
+      console.log("finishPurchase");
+      console.log(p);
+      console.log("finishPurchase status");
+      console.log(p.state);
+      localStorage.goldCoins = (localStorage.goldCoins | 0) + 10;
 
+      //if "state":"approved" the call server api to save purchase details
+      //if p.state()==approved
+      if (p.state == "approved") {
+        inAppPurchaseDone(p);
+      }
 
+      p.finish();
+    }
 
-// <!-- script for toggling display of the form -->
+    function inAppPurchaseDone(p) {
+      // localStorage.setItem("isSubscribed", "true");
+      showATab("dashboard");
 
-function toggleDisplay() {
-var x = document.getElementById("collapseStripe");
-if (x.style.display === "none") {
-    x.style.display = "block";
-} else {s
-    x.style.display = "none";
-}
-}
+      // NEED Figure out blurb to send from p to set as iap_burb which
+      // we need to coordinate unsubscribe interface.
 
+      // sets inform API user is now subscribe success :)
+      // XXX fix IAP blurb info
+      do_set_paying("P_IAP Info Blurb");
 
-function finishPurchase(p) {
-  console.log("finishPurchase");
-  console.log(p);
-  console.log("finishPurchase status");
-  console.log(p.state);
-  localStorage.goldCoins = (localStorage.goldCoins | 0) + 10;
-
-  //if "state":"approved" the call server api to save purchase details
-  //if p.state()==approved
-  if (p.state == "approved") {
-    inAppPurchaseDone(p);
-  }
-
-  p.finish();
-}
-
-function inAppPurchaseDone(p) {
-  // localStorage.setItem("isSubscribed", "true");
-  showATab("dashboard");
-
-  // NEED Figure out blurb to send from p to set as iap_burb which
-  // we need to coordinate unsubscribe interface.
-
-  // sets inform API user is now subscribe success :)
-  // XXX fix IAP blurb info
-  do_set_paying("P_IAP Info Blurb");
-
-  // $("#not-subscribed-user").hide();
-  // $("#subscribed-user").show();
-}
+      // $("#not-subscribed-user").hide();
+      // $("#subscribed-user").show();
+    }
 
 // function refreshUI() {
 //   const product = store.get("base_subscription_7");
@@ -173,3 +169,4 @@ function inAppPurchaseDone(p) {
 //     ${product.canPurchase ? button : ""}
 //     </div>`;
 // }
+}
