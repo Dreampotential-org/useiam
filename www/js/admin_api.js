534daebfb08b2390abbc6884bcc1a2e79a08a161
Aaron Rosen <aaronorosen@gmail.com>
2022-05-13 14:59:03 -0700

    update new design file for santosh

---
+++ www/js/admin_api.js
@@ -0,0 +1,39 @@
+function init() {
+
+    get_all_activity(function(events) {
+        for(var e of events['events']) {
+            console.log(e);
+        }
+        // populate screen....
+    })
+
+}
+
+
+function get_all_activity(callback) {
+  var settings = {
+    async: true,
+    crossDomain: true,
+    headers: {
+      Authorization: "Token " + localStorage.getItem("session_id")
+    },
+    url: SERVER + "/api/list-patient-events-v2/",
+    method: "GET",
+    processData: false,
+    contentType: false,
+    mimeType: "multipart/form-data"
+  };
+
+  $.ajax(settings).done(function(response) {
+      var msg= JSON.parse(response);
+      videoData =JSON.parse(response);
+      var allData = JSON.parse(response);
+      console.log(allData)
+      callback(msg);
+    }).fail(function(err) {
+      alert("Got err");
+      console.log(err);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
